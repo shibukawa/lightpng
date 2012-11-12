9a1298810e65328820c31616f7fccccf4f3d78a3
Yoshiki Shibukawa <yshibukawa@ngmoco.com>
2012-11-12 13:15:54 -0800

    Fix bug preview picture of ATC file format

--- no_opensource/lightpng.cpp
+++ no_opensource/lightpng.cpp
@@ -327,7 +327,7 @@ void parse_arg(int argc, const char** argv, const char*& input, output_list& out
                 std::string path(argv[i]);
                 if (check_ext(path, ".png"))
                 {
-                    outputs.push_back(output_type(ATCPlusHeaderFile, path));       
+                    outputs.push_back(output_type(PreviewATCFile, path));       
                 }
                 else
                 {
