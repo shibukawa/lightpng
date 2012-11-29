5259ed1c00ed803bea8246985550694776f93a1b
Yoshiki Shibukawa <yshibukawa@ngmoco.com>
2012-11-29 11:14:27 -0800

    fix build script

--- build.sh
+++ build.sh
@@ -16,7 +16,9 @@ mv lightpng build/lightpng_mac_opensource/
 cp LICENSE.rst build/lightpng_mac_opensource/
 cp README.rst build/lightpng_mac_opensource/
 cp README_JP.rst build/lightpng_mac_opensource/
-zip -9 -r build/lightpng_mac_opensource_`date +%Y%m%d`.zip build/lightpng_mac_opensource
+cd build
+zip -9 -r lightpng_mac_opensource_`date +%Y%m%d`.zip lightpng_mac_opensource
+cd ..
 
 scons --no-opensource
 mv lightpng build/lightpng_mac_noopensource/
@@ -24,12 +26,16 @@ cp LICENSE.rst build/lightpng_mac_noopensource/
 cp README.rst build/lightpng_mac_noopensource/
 cp README_JP.rst build/lightpng_mac_noopensource/
 cp LICENSE_PVRTexLib.rst build/lightpng_mac_noopensource/
-zip -9 -r build/lightpng_mac_noopensource_`date +%Y%m%d`.zip build/lightpng_mac_noopensource
+cd build
+zip -9 -r lightpng_mac_noopensource_`date +%Y%m%d`.zip lightpng_mac_noopensource
+cd ..
 
 scons --mingw32
 mv lightpng.exe build/lightpng_win_opensource/
 cp LICENSE.rst build/lightpng_win_opensource/
 cp README.rst build/lightpng_win_opensource/
 cp README_JP.rst build/lightpng_win_opensource/
-zip -9 -r build/lightpng_win_opensource_`date +%Y%m%d`.zip build/lightpng_win_opensource
+cd build
+zip -9 -r lightpng_win_opensource_`date +%Y%m%d`.zip lightpng_win_opensource
+cd ..
 
